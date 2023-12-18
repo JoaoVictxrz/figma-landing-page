01b370b6d172796fc52d29ec08ad15845cc5c1ec
João Victor <jvbatistagomes11@gmail.com>
2023-12-18 16:18:39 -0300

    feat: create third landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,10 +16,11 @@ export default function Home() {
           <button className="bg-third w-64 h-14 mt-8 rounded-3xl font-light text-xl shadow-xl hover:bg-primary">Ver mais</button>
         </div>
 
+        {/* Page - 2 */}
         <div className="bg-secondary w-full h-[945px] flex justify-between items-center">
           <div className="flex flex-col gap-5 m-32">
             <div className="w-16 h-2 bg-primary shadow-xl"></div>
-            <h1 className="text-black font-black text-7xl font-sans">O que <br />fazemos?</h1>
+            <h1 className="text-black font-black text-5xl font-sans">O que <br />fazemos?</h1>
             <h2 className="text-black/50  text-2xl font-bold font-sans">Somos uma Plataforma que conecta  empresas da área industrial que precisam contratar mão de Obra, e os melhores Profissionais do Mercado.</h2>
             <span className="text-black/50 text-xl font-light">A plataforma surgiu devido as dificuldades para encontrar Mão de Obra qualificada com rapidez e assertividade. Os processos de seleção ficam cada dia mais complexos e demorados, por isso resolvemos criar algo simples e funcional , onde qualquer pessoa com um celular na mão consegue anunciar, acompanhar e medir o alcance da vaga.</span>
           </div>
@@ -29,6 +30,23 @@ export default function Home() {
               height={2000}
               width={3500}
               alt="image-1" />
+          </div>
+        </div>
+
+        {/* Page - 3 */}
+        <div className="bg-secondary w-full h-[945px] flex justify-between items-center">
+          <div className="flex flex-col gap-5 m-32">
+            <div className="w-16 h-2 bg-primary shadow-xl"></div>
+            <h1 className="text-black font-black text-5xl font-sans">Para <span className="text-primary">empresas</span></h1>
+            <h2 className="text-black/50  text-xl font-bold font-sans">Encontre os melhores profissionais disponíveis no mercado.</h2>
+            <span className="text-black/50 text-2xl font-light">Unimos especialistas com mais de 20 anos de experiencia em diversas modalidade: <span className="text-fourth">Solda, Caldeiraria, Pintura, Mecânica etc</span>.. Adicionamos ao conhecimento deles Tecnologia e Inteligência Artificial, criando uma ferramenta com filtros mais precisos, onde só o profissionais que atendam os requisitos da vaga anunciada, podem se cadastrar. Evitando o desperdício de tempo e recursos.</span>
+          </div>
+          <div className="mr-24 ">
+            <Image
+              src="/image2.png"
+              height={2000}
+              width={3500}
+              alt="image-2" />
           </div>
         </div>
       </main >
