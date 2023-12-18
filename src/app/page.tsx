8824b1569d1601d57315541f9f0473f7b17a70ef
João Victor <jvbatistagomes11@gmail.com>
2023-12-18 15:59:17 -0300

    feat: create page-2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import { NavBar } from "./components/navbar";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <>
       <NavBar />
       <main >
-
-        <div className="bg-gradient-to-b from-primary via-primary to-secondary/70 w-full h-[833px] flex flex-col items-center justify-center">
+        {/* Inicial page */}
+        <div className="bg-gradient-to-b from-primary via-primary to-primary/95 w-full h-[833px] flex flex-col items-center justify-center">
           <h1 className="text-center">A plataforma que conecta</h1>
           <h1 className="text-7xl font-extrabold">empresas <span className="text-center">e</span> profissionais</h1>
           <h1 className="text-center">da Metalmecânica</h1>
@@ -15,6 +16,21 @@ export default function Home() {
           <button className="bg-third w-64 h-14 mt-8 rounded-3xl font-light text-xl shadow-xl hover:bg-primary">Ver mais</button>
         </div>
 
+        <div className="bg-secondary w-full h-[945px] flex justify-between items-center">
+          <div className="flex flex-col gap-5 m-32">
+            <div className="w-16 h-2 bg-primary shadow-xl"></div>
+            <h1 className="text-black font-black text-7xl font-sans">O que <br />fazemos?</h1>
+            <h2 className="text-black/50  text-2xl font-bold font-sans">Somos uma Plataforma que conecta  empresas da área industrial que precisam contratar mão de Obra, e os melhores Profissionais do Mercado.</h2>
+            <span className="text-black/50 text-xl font-light">A plataforma surgiu devido as dificuldades para encontrar Mão de Obra qualificada com rapidez e assertividade. Os processos de seleção ficam cada dia mais complexos e demorados, por isso resolvemos criar algo simples e funcional , onde qualquer pessoa com um celular na mão consegue anunciar, acompanhar e medir o alcance da vaga.</span>
+          </div>
+          <div className="mr-24 ">
+            <Image
+              src="/image1.png"
+              height={2000}
+              width={3500}
+              alt="image-1" />
+          </div>
+        </div>
       </main >
     </>
   )
